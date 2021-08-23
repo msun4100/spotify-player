710fa21b443bf264c6db4b862413d0d71fcb357c
msun4100 <msun4100@gmail.com>
2021-08-23 14:43:54 +0900

    feat: 뮤직앱 UI 초기 세팅

--- App.tsx
+++ App.tsx
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StatusBar } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+
+import Album from "./components/Album";
+import BottomTab from "./components/BottomTab";
+import LoadAssets from "./components/LoadAssets";
+
+const assets = [
+  require("./assets/Jan-Blomqvist.jpg"),
+  require("./assets/thebay.jpg"),
+];
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LoadAssets {...{ assets }}>
+      <StatusBar barStyle="light-content" />
+      <Album />
+      <BottomTab />
+    </LoadAssets>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
